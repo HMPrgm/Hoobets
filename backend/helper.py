dab3b343d6bbab72dd65ef8c8da6f245139369f4
Matan Morse <matanbryant@gmail.com>
2024-03-23 14:22:50 -0400

    fix requirements, create helper.py

--- backend/helper.py
+++ backend/helper.py
@@ -1,8 +1,10 @@
-from app import db
+from app import db, create_app
 from models import User
 NEW_CREDITS = 50
 
 def add_user(username, password, email):
-    user = User(email = email, credits = NEW_CREDITS, password = password, username = username)
-    db.session.add(user)
-    db.session.commit()
+    app = create_app()
+    with app.app_context():
+        user = User(email = email, credits = NEW_CREDITS, password = password, username = username)
+        db.session.add(user)
+        db.session.commit()
