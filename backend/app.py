34f079aab28b197eb08f565a1cdb279d80f1e287
Matan Morse <matanbryant@gmail.com>
2024-03-23 13:37:57 -0400

    create database schema

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,16 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.static_url_path="../frontend/public/templates"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite'
+
+    db.init_app(app)
     return app
+
 
 app = create_app()
 
---
+++ backend/models.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from app import db, app
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=True)
+    credits = db.Column(db.Integer, unique=True)
+
+class Bet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    desc = db.Column(db.String(80), unique=True, nullable=False)
+    start = db.Column(db.DateTime)
+    end = db.Column(db.DateTime)
+    active = db.Column(db.Boolean)
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    wagers = db.relationship('Wager', backref='bet')
+    options = db.relationship('Option', backref='bet')
+
+
+class Wager(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer)
+
+    better_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    option_id = db.Column(db.Integer, db.ForeignKey('option.id'))
+
+class Option(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    bet_id = db.Column(db.Integer, db.ForeignKey('bet.id'))
+    desc = db.Column(db.String(100))
+    value = db.Column(db.Integer)
+
+
+
+
+
+
+
+
+
+
+
+with app.app_context():
+    db.create_all()
