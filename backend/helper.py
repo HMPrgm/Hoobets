d097ee2930ed82f489987bd42525e1d0c6571659
Galen <galenbird1@gmail.com>
2024-03-23 15:02:48 -0400

    added closing out events

--- backend/helper.py
+++ backend/helper.py
@@ -27,5 +27,29 @@ def add_option(id, event_id, desc, value):
         option = Option(id = id, event_id = event_id, desc = desc, value = value)
         db.session.add(option)
         db.session.commit()
+
+def close_out_event(event_name, winning_option_desc):
+    with app.app_context():
+        event = Event.query.filter_by(name=event_name).first()
+        winning_option = Option.query.filter_by(event_id=event.id, desc=winning_option_desc).first()
+        losing_options = Option.query.filter(Option.event_id == event.id, Option.id != winning_option.id).all()
+        total_losing_credits = sum(option.value for option in losing_options)
+        losing_wagers = Wager.query.filter(Wager.event_id == event.id, Wager.option_id.in_([option.id for option in losing_options])).all()
+        winning_wagers = Wager.query.filter(Wager.event_id == event.id, Wager.option_id == winning_option.id).all()
+        for wager in winning_wagers:
+            wagering_user = wager.user
+            wager_amount = wager.amount
+            wagering_user.credits += wager_amount  # Return original bet
+            if total_losing_credits > 0:
+                proportion = wager_amount / total_losing_credits
+                proportionate_losers_credits = sum(option.value * proportion for option in losing_options)
+                wagering_user.credits += proportionate_losers_credits  # Additional credits from losers
+        for wager in losing_wagers:
+            wagering_user = wager.user
+            wagering_user.credits -= wager.amount
+        event.active = False
+        db.session.commit()
+        return total_losing_credits
+
         
 
