c87e11123c1125d331db5ca39306536ea8dddbaf
MSI Laptop <henrymunprof@gmail.com>
2024-03-23 14:36:50 -0400

    - Set Up bet preview carthage must be destroyed

---
+++ frontend/src/features/BetPreview.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BetPreview = ({bet}) => {
+    return (
+        <div className="bet-element" >
+            <div className="bet-element-header"><span className="bet-element-name">{bet.name}</span><span className="bet-element-likes">Likes: {bet.likes}</span></div>
+            <div className="bet-element-body">{bet.description}</div>
+        </div >
+      )
+};
+
+export default BetPreview;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,37 +1,78 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios'
+import BetPreview from "../features/BetPreview";
 
 const Home = () => {
     const [bets, setBets] = useState('');
 
     useEffect(()=>{
-        axios.post('/bets', {}, {
-            params: {
-                amount:10
-            }
+        axios.post('/bets', {
+            amount:10
         })
         .then(res => {
             setBets(res.data)
+            
         })
         .catch(e=>{
+            setBets([{
+                "id":1,
+                "active":1,
+                "name": "Professor Orrico Stickers",
+                "description": "Professor Orrico Stickers, renowned in academia for his pioneering work in cognitive psychology and human behavior, has recently emerged as a leading figure in the field of educational innovation. His eponymous stickers, designed with meticulous attention to detail and backed by rigorous empirical research, have revolutionized the way educators approach motivation and learning reinforcement. Unlike conventional stickers, Professor Orrico's creations are infused with carefully crafted affirmations and motivational messages, strategically tailored to boost students' self-esteem and foster a positive attitude towards learning.",
+                "image": "https://m.media-amazon.com/images/I/71wIaj8jt2L._AC_UF894,1000_QL80_.jpg",
+                "data": {
+                    "type": "overunder",
+                    "max":5.0,
+                    "min":0.0,
+                    "num":3.5
+                },
+                "likes":120
+            },
+            {
+                "id":2,
+                "active":0,
+                "name": "Professor Morrison Gets Pissed off",
+                "description": "Lorem Ipsum",
+                "image": "https://m.media-amazon.com/images/I/71wIaj8jt2L._AC_UF894,1000_QL80_.jpg",
+                "data": {
+                    "type": "happens",
+                    "max":10.0,
+                    "min":0.0,
+                    "num":5.5
+                },
+                "likes":121
+            },
+            {
+                "id":3,
+                "active":1,
+                "name": "Professor Mahmood",
+                "description": "Things can become pretty much complicated",
+                "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fasif-mahmood-5aa930a&psig=AOvVaw3Wf2mY7syoXZuoKuKZ74QV&ust=1711300728849000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMiMhJLyioUDFQAAAAAdAAAAABAE",
+                "data": {
+                    "type": "exist"
+                },
+                "likes":32
+            }]);
             console.error(e)
+
         })
     },[]);
+    
     try {
         return (
+            
+            
             bets.map((bet, i) => (
-                <li key={i}>
-                    <h2>{bet.name}</h2>
-                    <p>{bet.description}</p>
-                    <p>Likes: {bet.likes}</p>
-                </li>
+                <BetPreview bet={bet}/>
             ))
+        
         )
     }
     catch (e) {
         return(
             <p>"Loading..."</p>
         )
+        
         
     }
 };
