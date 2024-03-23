f45158bcee71450262a032991ccbbe53cf9cf204
Matan Morse <matanbryant@gmail.com>
2024-03-23 17:40:16 -0400

    connect home to database

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from flask_login import login_required
 main = Blueprint('auth', __name__)
 
@@ -10,4 +10,20 @@ def addevent():
     start = request.form.get('start')
     end = request.form.get('end')
     
+@main.route("/bets", methods=['Post'])
+def getEvents():
+    from models import Event
+    events = Event.query.all()
+    response = [] # list of dicts
+
+    for event in events:
+        dict = {}
+        dict['id'] = event.id
+        dict['active'] = 1 if True else 0
+        dict['name'] = event.name
+        dict['description'] = event.desc
+        dict['start'] = event.start
+        dict['end'] = event.end
+        response.append(dict)
     
+    return jsonify(response)
