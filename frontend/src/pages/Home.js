7374a1df55ef326a7ab37a29958dd14353a824c0
MSI Laptop <henrymunprof@gmail.com>
2024-03-23 13:37:36 -0400

    - Started working on Home Page carthage must be destroyed

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,9 +2,38 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios'
 
 const Home = () => {
-    return (
-        <h1>Hello, World!</h1>
-      )
+    const [bets, setBets] = useState('');
+
+    useEffect(()=>{
+        axios.post('/bets', {}, {
+            params: {
+                amount:10
+            }
+        })
+        .then(res => {
+            setBets(res.data)
+        })
+        .catch(e=>{
+            console.error(e)
+        })
+    },[]);
+    try {
+        return (
+            bets.map((bet, i) => (
+                <li key={i}>
+                    <h2>{bet.name}</h2>
+                    <p>{bet.description}</p>
+                    <p>Likes: {bet.likes}</p>
+                </li>
+            ))
+        )
+    }
+    catch (e) {
+        return(
+            <p>"Loading..."</p>
+        )
+        
+    }
 };
 
 export default Home;
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios'
 
 const Profile = ({user}) => {
+
     return (
         <h1>{user}</h1>
       )
