f0f4ad20e5b9fee54497ed8d843413754ed4d8be
Galen <galenbird1@gmail.com>
2024-03-23 14:37:09 -0400

    added helper.py

--- backend/helper.py
+++ backend/helper.py
@@ -1,10 +1,22 @@
 from app import db, create_app
-from models import User
+from models import User, Event, Wager
+from datetime import datetime
 NEW_CREDITS = 50
+app = create_app()
 
 def add_user(username, password, email):
-    app = create_app()
     with app.app_context():
         user = User(email = email, credits = NEW_CREDITS, password = password, username = username)
         db.session.add(user)
         db.session.commit()
+
+def add_bet(name, desc, end, creator_id):
+    with app.app_context():
+        bet = Bet(name = name, desc = desc, start = datetime.now, end = end, active = True, creator_id = creator_id)
+        db.session.add(bet)
+        db.session.commit()
+
+def add_wager(bettor_id, amount, option_id, bet_id):
+    with app.app_context:
+        wager = Wager(better_id = bettor_id)
+
