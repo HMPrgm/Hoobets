276bf956f8925cf7916b74591fe5a0bfece4e6a2
Matan Morse <matanbryant@gmail.com>
2024-03-23 15:00:36 -0400

    add main and auth blueprints. add user login

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from app import db, app
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
