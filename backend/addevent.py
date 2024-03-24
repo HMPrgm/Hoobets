0ca55f952b3d44752f57c6ceb9947d83051f8ab5
MSI Laptop <henrymunprof@gmail.com>
2024-03-24 00:46:12 -0400

    qweqweqewew

---
+++ backend/addevent.py
@@ -0,0 +1,6 @@
+from helper import add_event
+name = input('name: ')
+desc = input('desc: ')
+id = input('creator id: ')
+pivot = input('pivot: ')
+add_event(name=name, desc=desc, creator_id=id, pivot=pivot)
