3a119580b62d9d2bfa53f2ee3c91616d5fe4e97d
MSI Laptop <henrymunprof@gmail.com>
2024-03-23 23:03:58 -0400

    bets a

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,11 +1,27 @@
-import React, {useState, useEffect} from "react";
-import axios from 'axios'
+import React, { useState } from 'react';
 
-const Profile = ({user}) => {
+const Profile = () => {
 
-    return (
-        <h1>{user}</h1>
-      )
+  const [username, setUsername] = useState('')
+  const [bets, setBet] = useState('')
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Welcome, {username}!</h1>
+          <h2>Your Bets</h2>
+          <ul className="list-group">
+            {bets.map((bet, index) => (
+              <li key={index} className="list-group-item">
+                <strong>{bet.name}</strong> - {bet.description} tokens
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
