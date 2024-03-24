73da290f9978b9d21bc81fcca7209f1294ce0ce3
MSI Laptop <henrymunprof@gmail.com>
2024-03-24 05:39:16 -0400

    psuh

--- frontend/src/features/BetResult.js
+++ frontend/src/features/BetResult.js
@@ -11,6 +11,8 @@ const BetResult = ({ bet, bet2 }) => {
                             <h2 className="card-title text-center fs-2">Results</h2>
                             <p className="card-text text-muted text-center">{bet.name}</p>
                             <p className="card-text text-center">The resulting number <span className=" fw-bold">{bet2.actual}</span> was <span className=" fw-bold">{bet2.highlow === 1 ? "Higher" : "Lower"} </span>than<span className=" fw-bold"> {bet.pivot} </span></p>
+                            <p className="card-text text-center"> Total Winners: <span className=" fw-bold">{bet2.winning}</span></p> 
+                            <p className="card-text text-center"> Total Losers: <span className=" fw-bold">{bet2.losing}</span></p>
                             <div className="btn btn-dark card-text text-center d-flex align-items-center justify-content-center">
                                 <a className="nav-link" href="/">Back Home</a>
                             </div>
